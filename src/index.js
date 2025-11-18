830eeab5d34acbc808ab8434482967294aee8252
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2025-11-17 18:27:16 -0800

    Integrate Atatus monitoring with config and test error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import * as atatus from 'atatus-spa';
+
+atatus.config('eb1d31712f644482b95bf377641fb875').install();
+atatus.notify(new Error('Test Atatus Setup')); // Optional: verify it's working
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
