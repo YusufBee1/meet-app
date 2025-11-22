aa9760c2ef1380d933881f94f4300c3ea295c4d8
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2025-11-21 23:52:15 -0800

    Complete Exercise 4.9: PWA offline caching + service worker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import App from './App';
 import './index.css';
 
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('eb1d31712f644482b95bf377641fb875').install();
 atatus.notify(new Error('Test Atatus Setup')); // Optional: verify it's working
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+// Register the service worker
+serviceWorkerRegistration.register();
